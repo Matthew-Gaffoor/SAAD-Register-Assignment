0bf089740b500e6a1dba87e6d9516a3c93ec35b1
Matthew-Gaffoor <78423467+Matthew-Gaffoor@users.noreply.github.com>
2022-11-14 23:31:47 +0000

    imported all dependencies

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
-var http = require("http");
-var url = require("url");
+const express = require('express');
+const dotenv = require('dotenv').config()
 
-function start(handle, route) {
-    function onRequest(request, response) {
-        const baseURL =  request.protocol + '://' + request.headers.host + '/';
-        const theURL = new URL(request.url, baseURL);
-        var pathname = theURL.pathname;
-        var query = theURL.query;
-        var postData = "";
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-            console.log("Received more data: "+ postDataChunk);
-        });
-        request.addListener("end", function() {
-            route(handle, pathname, response, postData);
-        });
-    }
-http.createServer(onRequest).listen(3000);
-console.log("Server started");
-}
-exports.start = start;
+const app = express();
+
+app.listen(3000, ()=>console.log("Server started"))
+
+console.log("hello world");
+
+app.get('/', function (req, res){
+    res.render('index', {});
+});
